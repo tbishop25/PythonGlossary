328a42ddc5677aa10b13ef2858b82bc36c7b3d10
GabeAllen <gallen4@xula.edu>
2021-11-03 08:41:36 -0500

    Stage 1 complete (user can still input invalid letters)

--- VocabDrill.java
+++ VocabDrill.java
@@ -1,3 +1,7 @@
+package PythonGlossary;
+
+import javax.lang.model.type.ArrayType;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
@@ -6,7 +10,7 @@ public class VocabDrill{
         Scanner keyb = new Scanner(System.in);
         String filename = "pythonGlossary.csv";
         Map<String,String>vocabMap = readCsvFileIntoMap(filename);
-        //System.out.println(vocabMap);
+        askAQuesiton(vocabMap);
         
     }// main
     static Map<String,String>readCsvFileIntoMap(String filename){
@@ -30,6 +34,80 @@ public class VocabDrill{
         }
 
         return map;
+    }
+
+    static void askAQuesiton(Map<String,String>vocabMap){
+        ArrayList<String> words = new ArrayList<>();
+        ArrayList<String> definitions = new ArrayList<>();
+        //initializing lists from the map to make random selection easier
+        for(String x : vocabMap.keySet()){
+            words.add(x);
+        }
+        for(String x : vocabMap.values()){
+            definitions.add(x);
+        }
+        //making a list of the options to show the user
+        ArrayList<String> options = new ArrayList<>();
+        //choosing a random word as the "correct" word and 3 other "incorrect" words
+        int indexForRandomDefinition = new Random().nextInt(definitions.size());
+        String correctWord = words.get(indexForRandomDefinition);
+        options.add(correctWord);
+        //removing the correct and incorrect words from the list after choosing them, so they cannot be chosen again
+        words.remove(indexForRandomDefinition);
+        for (int i = 0; i < 3; i++) {
+            int indexForIncorrectWord = new Random().nextInt(definitions.size());
+            String falseWord = words.get(indexForIncorrectWord);
+            options.add(falseWord);
+            words.remove(indexForIncorrectWord);
+        }
+
+        System.out.println("Which of the following means: "+definitions.get(indexForRandomDefinition));
+        //randomizing the output of the options and printing
+        Collections.shuffle(options);
+        char letter = 65;//This is to print A, B, C, and D in font of the options
+        for(int i = 0; i<4;i++){
+            System.out.println(letter+") "+options.get(i));
+            letter++;
+        }
+        //asking user for an answer
+        Scanner input = new Scanner(System.in);
+        String userInput = input.next().toUpperCase();
+        if(!Character.isLetter(userInput.charAt(0))&&(!userInput.equals("A"))){
+            System.out.println("Invalid input. Please select 'A','B','C', or 'D'");
+            userInput = input.next().toUpperCase();
+        }
+        //Check to see if the user got the question right
+        switch(userInput){
+                case "A":
+                    if(options.get(0).equals(correctWord)){
+                        System.out.println("correct");
+                    }else{
+                        System.out.println("incorrect. The right answer was: "+correctWord);
+                    }
+                    break;
+                case "B":
+                    if(options.get(1).equals(correctWord)){
+                        System.out.println("correct");
+                    }else{
+                        System.out.println("incorrect. The right answer was: "+correctWord);
+                    }
+                    break;
+                case "C":
+                    if(options.get(2).equals(correctWord)){
+                        System.out.println("correct");
+                    }else{
+                        System.out.println("incorrect. The right answer was: "+correctWord);
+                    }
+                    break;
+                case "D":
+                    if(options.get(3).equals(correctWord)){
+                        System.out.println("correct");
+                    }else{
+                        System.out.println("incorrect. The right answer was: "+correctWord);
+                    }
+                    break;
+            }
+
     }
     
 }// class
